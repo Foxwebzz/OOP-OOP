76243c8eab02dec1389fa85f990b785a52ec581d
DESKTOP-UG3PI5T\Novakovic <nikola2312017@gmail.com>
2022-01-23 22:29:34 +0100

    note Nikola

---
+++ nikola.js
@@ -0,0 +1,25 @@
+// let app = {
+//     state = {
+//         name : "Nikola"
+//     }
+// }
+
+
+// console.log();
+
+// const person = () => {
+//     let newPerson = {
+//         name: "nikola"
+//     }
+
+// }
+// console.log(person.newPerson);
+// person()
+
+let person = {
+    newPerson: {
+        name: "Danilo"
+    }
+}
+console.log(person.newPerson.name);
+
